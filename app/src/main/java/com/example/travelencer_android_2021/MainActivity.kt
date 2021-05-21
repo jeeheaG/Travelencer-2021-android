f37b9430fe53e1f68930754c125a2737c4ea950e
jeehea <jeehea9987@gmail.com>
2021-05-22 01:26:19 +0900

    jeehea 브랜치 첫 커밋

--- app/src/main/java/com/example/travelencer_android_2021/MainActivity.kt
+++ app/src/main/java/com/example/travelencer_android_2021/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.travelencer_android_2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//jeehea브랜치 첫 커밋
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

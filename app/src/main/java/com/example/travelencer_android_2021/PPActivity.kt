c9309ad8dc47696e4e90503d7b8a9926f974fb7d
minWachya <alal7272@gmail.com>
2021-05-22 15:11:45 +0900

    개인정보 보호정책, 서비스 이용약관 액티비티 추가

---
+++ app/src/main/java/com/example/travelencer_android_2021/PPActivity.kt
@@ -0,0 +1,12 @@
+package com.example.travelencer_android_2021
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+// 개인정보 보호정책 액티비티
+class PPActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pp)
+    }
+}
---
+++ app/src/main/java/com/example/travelencer_android_2021/TOSActivity.kt
@@ -0,0 +1,12 @@
+package com.example.travelencer_android_2021
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+// 서비스 이용 약관 액티비티
+class TOSActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tos)
+        }
+}

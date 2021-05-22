fa797a6af1ce8d60108ee66f79d8430cff0f2200
minWachya <alal7272@gmail.com>
2021-05-22 09:56:49 +0900

    test

---
+++ app/src/main/java/com/example/travelencer_android_2021/Splash.kt
@@ -0,0 +1,17 @@
+package com.example.travelencer_android_2021
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+// 스플래시 화면
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 로그인 액티비티로 이동
+        //val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+        //startActivity(intent)
+        //finish()
+    }
+}

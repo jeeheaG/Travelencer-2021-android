bc5a730291a71db8e6467f5019b738bce97561bf
jeehea <jeehea9987@gmail.com>
2021-06-04 17:03:32 +0900

    네비바 확정

--- app/src/main/java/com/example/travelencer_android_2021/PlaceFilterFragment.kt
+++ app/src/main/java/com/example/travelencer_android_2021/PlaceFilterFragment.kt
@@ -1,4 +1,16 @@
 package com.example.travelencer_android_2021
 
-class PlaceFilterFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class PlaceFilterFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_place_filter, container, false)
+
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 }

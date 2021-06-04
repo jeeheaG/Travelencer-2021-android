7be005adf64752787df86e6dc7b85e8eacbe1f00
jeehea <jeehea9987@gmail.com>
2021-06-04 17:26:26 +0900

    test

--- app/src/main/java/com/example/travelencer_android_2021/PlaceFilterFragment.kt
+++ app/src/main/java/com/example/travelencer_android_2021/PlaceFilterFragment.kt
@@ -5,7 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-
+////
 class PlaceFilterFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_place_filter, container, false)

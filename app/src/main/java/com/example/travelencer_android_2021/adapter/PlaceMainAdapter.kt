663789959d71d8aeeeb8a759db5f575fb38115a1
jeehea <jeehea9987@gmail.com>
2021-06-15 01:30:13 +0900

    PlaceDetail 화면 연결

--- app/src/main/java/com/example/travelencer_android_2021/adapter/PlaceMainAdapter.kt
+++ app/src/main/java/com/example/travelencer_android_2021/adapter/PlaceMainAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.travelencer_android_2021.adapter
 
+import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
@@ -9,18 +11,20 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.example.travelencer_android_2021.PlaceDetailActivity
 import com.example.travelencer_android_2021.R
 import com.example.travelencer_android_2021.model.ModelPlaceMain
 
-class PlaceMainAdapter(val placeList: ArrayList<ModelPlaceMain>): RecyclerView.Adapter<PlaceMainAdapter.CustomViewHolder>() {
+class PlaceMainAdapter(val placeList: ArrayList<ModelPlaceMain>, val mContext: Context): RecyclerView.Adapter<PlaceMainAdapter.CustomViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceMainAdapter.CustomViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item_place_main, parent, false)
         return CustomViewHolder(view).apply {
             itemView.setOnClickListener {
                 val curPosition: Int = adapterPosition
-                val place: ModelPlaceMain = placeList.get(curPosition)
+                val place: ModelPlaceMain = placeList[curPosition]
                 Toast.makeText(parent.context, "이름:${place.name}, 위치:${place.loc}, 설명:${place.explain}", Toast.LENGTH_SHORT).show()
             }
         }
@@ -28,12 +32,25 @@ class PlaceMainAdapter(val placeList: ArrayList<ModelPlaceMain>): RecyclerView.A
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onBindViewHolder(holder: PlaceMainAdapter.CustomViewHolder, position: Int) {
-        holder.img.setImageResource(placeList.get(position).img)
-        holder.name.text = placeList.get(position).name
-        holder.loc.text = placeList.get(position).loc
-        holder.explain.text = placeList.get(position).explain.substring(0,20).plus("...") //설명부분 문자열 자르고 ...붙이기
+        holder.img.setImageResource(placeList[position].img)
+        holder.name.text = placeList[position].name
+        holder.loc.text = placeList[position].loc
+        holder.explain.text = placeList[position].explain.substring(0,20).plus("...") //설명부분 문자열 자르고 ...붙이기
 
         holder.img.clipToOutline = true //안드로이드 버전 5 (롤리팝) 이상부터 적용
+
+        //clickListener()
+
+/*        holder.itemView.setOnClickListener{
+            val intent = Intent(it.context, PlaceDetailActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            it.context.startActivity(intent)
+        }*/
+        holder.itemView.setOnClickListener{
+            val intent = Intent(mContext, PlaceDetailActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            mContext.startActivity(intent)
+        }
     }
     override fun getItemCount(): Int {
         return placeList.size
@@ -45,6 +62,15 @@ class PlaceMainAdapter(val placeList: ArrayList<ModelPlaceMain>): RecyclerView.A
         val name = itemView.findViewById<TextView>(R.id.tvPlaceItemName) //장소 이름
         val loc = itemView.findViewById<TextView>(R.id.tvPlaceItemLoc) //지역
         val explain = itemView.findViewById<TextView>(R.id.tvPlaceItemExplain) //설명
+
+/*        fun clickListener(itemView: View){
+            itemView.setOnClickListener{
+                val intent = Intent(it.context, PlaceDetailActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                it.context.startActivity(intent)
+            }
+        }*/
+
     }
 
 }
--- app/src/main/java/com/example/travelencer_android_2021/adapter/PostBlogAdapter.kt
+++ app/src/main/java/com/example/travelencer_android_2021/adapter/PostBlogAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.travelencer_android_2021.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup

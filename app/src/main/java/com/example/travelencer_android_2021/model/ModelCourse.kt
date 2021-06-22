5384c4d07c94ed1abc95b2badee62d1009bf1fd6
minWachya <alal7272@gmail.com>
2021-06-22 17:07:54 +0900

    변수명 변경

--- app/src/main/java/com/example/travelencer_android_2021/model/ModelCourse.kt
+++ app/src/main/java/com/example/travelencer_android_2021/model/ModelCourse.kt
@@ -1,4 +1,4 @@
 package com.example.travelencer_android_2021.mdodel
 
-// 코스 모델(식당 이름, 지역 이름, 날짜)
-data class ModelCourse (val foodName : String, val areaName : String, val date : String)
+// 코스 모델(관광지나 맛집 이름, 지역 이름, 날짜)
+data class ModelCourse (val courseName : String, val areaName : String, val date : String)

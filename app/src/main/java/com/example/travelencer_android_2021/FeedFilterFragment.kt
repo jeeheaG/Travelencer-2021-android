e705450251b54baaeb719ba0f420146cc0db0e51
minWachya <alal7272@gmail.com>
2021-06-22 13:44:02 +0900

    피드 필터 xml 수정 & 설정 프레그먼트 - 사진 선택

--- app/src/main/java/com/example/travelencer_android_2021/FeedFilterFragment.kt
+++ app/src/main/java/com/example/travelencer_android_2021/FeedFilterFragment.kt
@@ -5,10 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.example.travelencer_android_2021.databinding.FragmentPlaceFilterBinding
 import kotlinx.android.synthetic.main.fragment_feed_filter.view.*
 
 // 여행 피드 - 필터
 class FeedFilterFragment : Fragment() {
+    private var _binding: FragmentPlaceFilterBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -17,11 +24,54 @@ class FeedFilterFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_feed_filter, container, false)
+        //val view = inflater.inflate(R.layout.fragment_feed_filter, container, false)
+        _binding = FragmentPlaceFilterBinding.inflate(inflater, container, false)
+        val view = binding.root
 
+        //지역 아이템 목록 더미 데이터
+        val placeLargeItemList: Array<String> = resources.getStringArray(R.array.place_large_item_list)
+        val placeSmallItemList: Array<String> = resources.getStringArray(R.array.place_small_item_list)
+
+        //어댑터. 프래그먼트이므로 context가져올 때 activity가 null인지 아닌지 확인
+        val PlaceLargeAdapter = activity?.let{ ArrayAdapter(it, R.layout.support_simple_spinner_dropdown_item, placeLargeItemList) }
+        val PlaceSmallAdapter = activity?.let{ ArrayAdapter(it, R.layout.support_simple_spinner_dropdown_item, placeSmallItemList) }
+
+        binding.spinPlaceLarge.adapter = PlaceLargeAdapter
+        binding.spinPlaceLarge.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                if(position!=0){
+                    Toast.makeText(activity, placeLargeItemList[position], Toast.LENGTH_SHORT).show()
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+
+        binding.spinPlaceSmall.adapter = PlaceSmallAdapter
+        binding.spinPlaceSmall.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                if(position!=0){
+                    Toast.makeText(activity, placeSmallItemList[position], Toast.LENGTH_SHORT).show()
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+
+        // 검색 버튼 눌렀을 때
+//        binding.btnSearch.setOnClickListener {
+//            val parentManager: FragmentManager = parentFragmentManager
+//            val pft: FragmentTransaction = parentManager.beginTransaction()
+//
+//            pft.add(R.id.flContainer, PlaceMainFragment(), TAG_PLACE_MAIN)
+//
+//            val placeFilter = parentManager.findFragmentByTag(TAG_PLACE_FILTER)
+//            val placeMain = parentManager.findFragmentByTag(TAG_PLACE_MAIN)
+//
+//            placeFilter?.let {pft.remove(it)}
+//
+//            pft.commitAllowingStateLoss()
+//        }
+//
         return view
     }
 
-    companion object {
-    }
 }
--- app/src/main/java/com/example/travelencer_android_2021/SettingFragment.kt
+++ app/src/main/java/com/example/travelencer_android_2021/SettingFragment.kt
@@ -1,9 +1,12 @@
 package com.example.travelencer_android_2021
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.OvalShape
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -11,18 +14,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import com.example.travelencer_android_2021.databinding.FragmentSettingBinding
+import kotlinx.android.synthetic.main.fragment_setting.*
 import kotlinx.android.synthetic.main.fragment_setting.view.*
 
 // 설정 프레그먼트
 class SettingFragment : Fragment() {
     private var mBinding : FragmentSettingBinding? = null
+    var uri : Uri? = null   // 이미지 파일 경로
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        //val view = inflater.inflate(R.layout.fragment_setting, container, false)
 
         val binding = FragmentSettingBinding.inflate(inflater, container, false)
         mBinding = binding
@@ -34,6 +39,26 @@ class SettingFragment : Fragment() {
         }
 
         imgProfile.clipToOutline = true //안드로이드 버전 5(롤리팝)이상에서만 적용
+
+        // <프로필 사진 변경> 버튼 클릭하면 갤러리에서 사진 가져오기
+        binding.btnLoingAndRegister.setOnClickListener {
+            getFromAlbum()
+        }
+
+        // <수정 완료> 버튼 클릭
+        binding.btnSetting.setOnClickListener {
+            Toast.makeText(context, "수정 완료되었습니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        // <로그아웃> 버튼 클릭
+        binding.btnLogout.setOnClickListener {
+            Toast.makeText(context, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        // <로그아웃> 버튼 클릭
+        binding.btnChatBot.setOnClickListener {
+            Toast.makeText(context, "챗봇 준비중", Toast.LENGTH_SHORT).show()
+        }
 
         // 뒤로가기 이미지 클릭
         binding.imgBack.setOnClickListener {
@@ -55,7 +80,36 @@ class SettingFragment : Fragment() {
         }
 
         return mBinding?.root
-        //return view
+    }
+
+    // 갤러리 이미지 선택해서 가져오기
+    fun getFromAlbum() {
+        val intent = Intent("android.intent.action.GET_CONTENT")
+        intent.type = "image/*"     // 모든 이미지
+        // 이미지 선택하면 응답 102 보내기
+        startActivityForResult(intent, 100) // 100 말고 다른 숫자도 가능
+    }
+
+    // 읍답 받은 액티비티
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // 이미지 잘 선택했으면
+        when(requestCode) {
+            100 -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    if (Build.VERSION.SDK_INT >= 19) {
+                        uri = data?.data    // 선택한 이미지의 주소
+                        // 사용자가 이미지를 선택했으면(null이 아니면)
+                        if (uri != null) {
+                            // 이미지 파일 읽어와서 출력하기
+                            val bitmap = BitmapFactory.decodeStream(activity?.contentResolver!!.openInputStream(uri!!))
+                            imgProfile.setImageBitmap(bitmap)
+                        }
+                    }
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {

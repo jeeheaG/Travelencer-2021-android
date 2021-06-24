a1d305cbb8f975081f833327cc9a92e0786f5e0a
minWachya <alal7272@gmail.com>
2021-06-24 16:44:59 +0900

    프로필 사진 크롭 추가

--- app/src/main/java/com/example/travelencer_android_2021/SettingFragment.kt
+++ app/src/main/java/com/example/travelencer_android_2021/SettingFragment.kt
@@ -1,7 +1,13 @@
 package com.example.travelencer_android_2021
 
+import android.Manifest
 import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.drawable.ShapeDrawable
@@ -9,6 +15,7 @@ import android.graphics.drawable.shapes.OvalShape
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -16,9 +23,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import com.example.travelencer_android_2021.databinding.FragmentSettingBinding
+import com.theartofdev.edmodo.cropper.CropImage
+import com.theartofdev.edmodo.cropper.CropImageView
 import kotlinx.android.synthetic.main.fragment_setting.*
 import kotlinx.android.synthetic.main.fragment_setting.view.*
+
+private const val PICK_FROM_ALBUM = 0
 
 // 설정 프레그먼트
 class SettingFragment : Fragment() {
@@ -79,6 +92,19 @@ class SettingFragment : Fragment() {
             startActivity(intent)
         }
 
+        // <회원 탈퇴> 텍스트뷰 클릭
+        binding.tvWithDraw.setOnClickListener {
+            AlertDialog.Builder(context)
+                    .setMessage("정말 탈퇴하시겠습니까?")
+                    .setPositiveButton("네") { dialog, which ->
+                        Toast.makeText(context, "탈퇴되었습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                    .setNegativeButton("아니오") { dialog, which ->
+                        Toast.makeText(context, "취소되었습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                    .show()
+        }
+
         return mBinding?.root
     }
 
@@ -86,30 +112,49 @@ class SettingFragment : Fragment() {
     fun getFromAlbum() {
         val intent = Intent("android.intent.action.GET_CONTENT")
         intent.type = "image/*"     // 모든 이미지
-        // 이미지 선택하면 응답 102 보내기
-        startActivityForResult(intent, 100) // 100 말고 다른 숫자도 가능
+        startActivityForResult(intent, PICK_FROM_ALBUM)
+        Log.d("mmm ddk", CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE.toString())
+    }
+
+    // 사진 크롭하기
+    private fun cropImage(uri: Uri?) {
+        CropImage.activity(uri).setGuidelines(CropImageView.Guidelines.ON)  // 크롭 위한 가이드 열어서 크롭할 이미지 받아오기
+                .setCropShape(CropImageView.CropShape.RECTANGLE)            // 사각형으로 자르기
+                .start(activity as NaviActivity, this@SettingFragment)
     }
 
     // 읍답 받은 액티비티
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data);
 
         // 이미지 잘 선택했으면
         when(requestCode) {
-            100 -> {
+            // 앨범에서 사진 가져오기
+            PICK_FROM_ALBUM -> {
                 if (resultCode == Activity.RESULT_OK) {
                     if (Build.VERSION.SDK_INT >= 19) {
                         uri = data?.data    // 선택한 이미지의 주소
                         // 사용자가 이미지를 선택했으면(null이 아니면)
                         if (uri != null) {
-                            // 이미지 파일 읽어와서 출력하기
-                            val bitmap = BitmapFactory.decodeStream(activity?.contentResolver!!.openInputStream(uri!!))
-                            imgProfile.setImageBitmap(bitmap)
+                            cropImage(uri)
                         }
                     }
                 }
             }
+            // 크롭해서 프로필 사진 설정하기
+            CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
+                val result = CropImage.getActivityResult(data)
+                if(resultCode == Activity.RESULT_OK){
+                    result.uri?.let {
+                        // 이미지 파일 읽어와서 설정하기
+                        val bitmap = BitmapFactory.decodeStream(activity?.contentResolver!!.openInputStream(result.uri!!))
+                        imgProfile.setImageBitmap(bitmap)
+
+                    }
+                }
+            }
         }
+
     }
 
     override fun onDestroyView() {

71759affd65e7e484aa08609aa3ebde60522ed84
minWachya <alal7272@gmail.com>
2021-07-01 21:31:24 +0900

    메인 액티비티 연결

--- app/src/main/java/com/example/travelencer_android_2021/MainActivity.kt
+++ app/src/main/java/com/example/travelencer_android_2021/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.travelencer_android_2021
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -20,9 +21,31 @@ class MainActivity : AppCompatActivity() {
 
         // 설정 이미지 클릭
         imgSetting.setOnClickListener {
-
+            setNavi(R.id.settingFragment)
         }
 
+        // <여행지 검색> 클릭
+        btnSearchPlace.setOnClickListener {
+            setNavi(R.id.placeMainFragment)
+        }
+
+        // <여행 피드> 클릭
+        btnFeed.setOnClickListener {
+            setNavi(R.id.feedFragment)
+        }
+
+        // <나의 여행 일지> 클릭
+        btnMyPost.setOnClickListener {
+            setNavi(R.id.postBlogFragment)
+        }
+
+    }
+
+    fun setNavi(fregmentId : Int) {
+        val outIntent = Intent(applicationContext, NaviActivity::class.java)
+        outIntent.putExtra("select", fregmentId)
+        setResult(Activity.RESULT_OK, outIntent)
+        finish()
     }
 
 
--- app/src/main/java/com/example/travelencer_android_2021/Splash.kt
+++ app/src/main/java/com/example/travelencer_android_2021/Splash.kt
@@ -20,8 +20,8 @@ class Splash : AppCompatActivity() {
         val mainExecutor : Executor = ContextCompat.getMainExecutor(this@Splash)
         backgroundExecutable.schedule({
             mainExecutor.execute {
-                // MainActivity 넘어가기
-                var intent = Intent(this@Splash, MainActivity::class.java)
+                // NaviActivity 넘어가기
+                var intent = Intent(this@Splash, NaviActivity::class.java)
                 startActivity(intent)
                 finish()
             }

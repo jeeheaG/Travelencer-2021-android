49371c637248930f50af1df1643be8d0ed9dc824
unknown <jeehea99@naver.com>
2021-07-07 21:42:45 +0900

    저장소 옮김

--- app/src/main/java/com/example/travelencer_android_2021/PostWriteActivity.kt
+++ app/src/main/java/com/example/travelencer_android_2021/PostWriteActivity.kt
@@ -3,7 +3,7 @@ package com.example.travelencer_android_2021
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.travelencer_android_2021.databinding.ActivityPostWriteBinding
-//뷰바인딩 사용
+//뷰바인딩 사용 //
 
 class PostWriteActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPostWriteBinding

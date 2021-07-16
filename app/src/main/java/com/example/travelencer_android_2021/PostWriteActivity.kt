f56f84e9be250438c71a3e74d5be1d492da86035
unknown <jeehea99@naver.com>
2021-07-16 23:57:12 +0900

    코스 추가 일시 입력받는 부분 구현

--- app/src/main/java/com/example/travelencer_android_2021/PostWriteActivity.kt
+++ app/src/main/java/com/example/travelencer_android_2021/PostWriteActivity.kt
@@ -8,6 +8,9 @@ import com.example.travelencer_android_2021.databinding.ActivityPostWriteBinding
 import java.util.*
 
 //뷰바인딩 사용
+//TODO : 다른 액티비티로 이동해서 입력한 정보를 받아서 글 작성 화면으로 돌아올 때
+// 작성하던 글을 유지하면서 방금 받은 정보를 추가해서 띄우려면.. 작성하던 내용을 sharedPreference에 저장했다가 불러와야하나?
+// 아니면 onActivityResult?
 
 class PostWriteActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPostWriteBinding
@@ -29,26 +32,32 @@ class PostWriteActivity : AppCompatActivity() {
 
         //여행 기간 입력
         binding.btnPostWriteStartDate.setOnClickListener {
-
             val startDateListener = DatePickerDialog.OnDateSetListener { view, y, m, d ->
                 binding.tvPostWriteStartDate.text = "${y} ${m+1} ${d}"
             }
-
             val startDatePicker = DatePickerDialog(this, startDateListener, year, month, day)
             startDatePicker.show()
         }
         binding.btnPostWriteEndDate.setOnClickListener {
-
             val endDateListener = DatePickerDialog.OnDateSetListener { view, y, m, d ->
                 binding.tvPostWriteEndDate.text = "${y} ${m+1} ${d}"
             }
-
             val endDatePicker = DatePickerDialog(this, endDateListener, year, month, day)
             endDatePicker.show()
         }
 
         binding.btnPostWriteAddPlace.setOnClickListener {
             val intent = Intent(this, PostWritePlaceActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnPostWriteAddCourse.setOnClickListener {
+            val intent = Intent(this, PostWriteCourseActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnPostWritePost.setOnClickListener {
+            val intent = Intent(this, PostDetailActivity::class.java)
             startActivity(intent)
         }
 
--- app/src/main/java/com/example/travelencer_android_2021/PostWriteCourseActivity.kt
+++ app/src/main/java/com/example/travelencer_android_2021/PostWriteCourseActivity.kt
@@ -1,8 +1,13 @@
 package com.example.travelencer_android_2021
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.travelencer_android_2021.databinding.ActivityPostWriteCourseBinding
+import java.sql.Time
+import java.time.Clock
+import java.util.*
 
 class PostWriteCourseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPostWriteCourseBinding
@@ -13,6 +18,45 @@ class PostWriteCourseActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        //현재 날짜, 시간
+        val calendar = Calendar.getInstance()
+        var year = calendar.get(Calendar.YEAR)
+        var month = calendar.get(Calendar.MONTH) //JANUARY == 0 임
+        var day = calendar.get(Calendar.DAY_OF_MONTH)
+        var hour = calendar.get(Calendar.HOUR)
+        var minute = calendar.get(Calendar.MINUTE)
 
+
+        binding.btnPostWriteCourseDate.setOnClickListener {
+            val dateListener = DatePickerDialog.OnDateSetListener { view, y, m, d ->
+                binding.tvPostWriteCourseDate.text = "${y} ${m+1} ${d}"
+            }
+            val datePicker = DatePickerDialog(this, dateListener, year, month, day)
+            datePicker.show()
+        }
+
+        binding.btnPostWriteCourseTime.setOnClickListener {
+            val timeListener = TimePickerDialog.OnTimeSetListener { view, h, m ->
+                var noon = "error"
+                var noonH = 0
+                if(h<12){
+                    noon = "오전"
+                    noonH = h
+                }
+                else if(h>=12){
+                    noon = "오후"
+                    noonH = h-12
+                }
+                binding.tvPostWriteCourseTime.text = "${noon} ${noonH}시 ${m}분"
+            }
+            val timePicker = TimePickerDialog(this, timeListener, hour, minute, true)
+            timePicker.show()
+        }
+
+
+
+        binding.ivBack.setOnClickListener{
+            finish()
+        }
     }
 }

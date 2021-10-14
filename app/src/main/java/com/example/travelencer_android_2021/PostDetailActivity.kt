d158aa68a6098727e98d79649464efd4c1c78cc2
pabaep <sewon225@gmail.com>
2021-10-14 23:31:41 +0900

    돌려놨어요

--- app/src/main/java/com/example/travelencer_android_2021/PostDetailActivity.kt
+++ app/src/main/java/com/example/travelencer_android_2021/PostDetailActivity.kt
@@ -3,7 +3,6 @@ package com.example.travelencer_android_2021
 import android.content.Intent
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.OvalShape
-import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,15 +11,11 @@ import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import com.example.travelencer_android_2021.adapter.PostBlogAdapter
-import com.example.travelencer_android_2021.adapter.PostBlogPhotoAdapter
 import com.example.travelencer_android_2021.adapter.PostDetailPhotoAdapter
 import com.example.travelencer_android_2021.adapter.PostDetailPlaceAdapter
 import com.example.travelencer_android_2021.course.CourseMaker
 import com.example.travelencer_android_2021.databinding.ActivityPostDetailBinding
-import com.example.travelencer_android_2021.model.ModelCasePhotoOnly
-import com.example.travelencer_android_2021.model.ModelPostBlog
-import com.example.travelencer_android_2021.model.ModelPostBlogPhoto
+import com.example.travelencer_android_2021.model.ModelFeedPhoto
 import com.example.travelencer_android_2021.model.ModelPostDetailPlace
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -40,14 +35,12 @@ class PostDetailActivity : AppCompatActivity() {
 
     private lateinit var storage : FirebaseStorage
     private lateinit var storageRef : StorageReference
+    private lateinit var postDetailPhotoAdapter : PostDetailPhotoAdapter
+    private lateinit var postDetailPlaceAdapter : PostDetailPlaceAdapter
 
     var auth: FirebaseAuth = FirebaseAuth.getInstance()
     var firestore : FirebaseFirestore = FirebaseFirestore.getInstance()
 
-    var photoList = arrayListOf<ModelPostBlogPhoto>()
-    var placeList = arrayListOf<ModelPostDetailPlace>()
-    //var ModelPostDetailPlace = ModelPostDetailPlace()
-    private lateinit var PostDetailPhotoAdapter : PostDetailPhotoAdapter
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,9 +48,15 @@ class PostDetailActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        PostDetailPhotoAdapter = PostDetailPhotoAdapter(photoList,this)
         storage = Firebase.storage
         storageRef = storage.reference
+
+        // PostDetailPhotoAdapter 어댑터 달기
+        postDetailPhotoAdapter = PostDetailPhotoAdapter(applicationContext)
+        binding.rvPostDetailPhotoList.adapter = postDetailPhotoAdapter
+        // PostDetailPlaceAdapter 어댑터 달기
+        postDetailPlaceAdapter = PostDetailPlaceAdapter()
+        binding.rvPostDetailPlaceList.adapter = postDetailPlaceAdapter
 
         val postId = intent.getStringExtra("postId") ?: "오류"
         val uid = intent.getStringExtra("uid").toString()
@@ -74,55 +73,14 @@ class PostDetailActivity : AppCompatActivity() {
             // 프로필 설정
             setProPic(uid)
 
-            /*// 사진 정보 가져오기
-            val postPhotoList = getPlacePhoto(postId)
-            Log.d(TAG, postPhotoList.toString())
-            // 사진 uri 가져오기
-            val postPhotoUri = getPhotoUri(postPhotoList)
-            Log.d(TAG, postPhotoUri.toString())*/
+            // 사진 정보 가져오기 + 설정
+            getPlacePhoto(postId)
+
+            // 장소 정보 가져오기 + 설정
+            getPlace(postId)
         }
 
 
-        //TODO: 기타 정보 불러오기
-        val placeList = arrayListOf(
-                ModelPostDetailPlace(R.drawable.ic_location_yellow, "수원 화성", "경기도 수원시"),
-                ModelPostDetailPlace(R.drawable.ic_location_yellow, "해우재", "경기도 수원시")
-        )
-        /*
-        val dummyImageUrl = Uri.parse("https://postfiles.pstatic.net/MjAyMTEwMTRfNDYg/MDAxNjM0MjE3MzA4MjUz.r0l1GOscNjIqf295t0nz5tzKMlzKXy5VoZWMsBI1Ev0g.wSWQDM7JRdxciFe73vRkTwOdx06AOKxM40bLkRXflTkg.JPEG.sewon225/dummy_haewoojae.jpg?type=w773")
-        val photoList = arrayListOf(
-                ModelPostBlogPhoto(dummyImageUrl)
-
-        )*/
-        //uid별 작성 게시글 불러오기
-        firestore?.collection("postT").whereEqualTo("uid",uid).get()
-                .addOnSuccessListener { docs->
-                    for(doc in docs){
-                        var postId = doc.data?.get("postId").toString()
-
-                        firestore.collection("postPhotoT").whereEqualTo("postId",postId).get()
-                                .addOnSuccessListener { docs3->
-                                    photoList.clear()
-                                    for (doc3 in docs3){
-                                        storage?.reference?.child("post")?.child(doc3.data?.get("postPhoto").toString())?.downloadUrl
-                                                ?.addOnSuccessListener { uri->
-                                                    photoList.add(ModelPostBlogPhoto(uri))
-                                                    PostDetailPhotoAdapter.notifyDataSetChanged()
-                                                }
-                                        //스토리지 호출 uri로 바꾸기
-
-                                    }
-
-                                    //사진 리스트 어뎁터 연결
-                                    /*ModelPostBlog.photoList = photoList
-                                    postList.add(ModelPostBlog)
-                                    postAdapter.notifyDataSetChanged()*/
-
-
-                                }
-
-                    }
-                }
 
         binding.rvPostDetailPlaceList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         binding.rvPostDetailPhotoList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
@@ -130,8 +88,7 @@ class PostDetailActivity : AppCompatActivity() {
         binding.rvPostDetailPlaceList.setHasFixedSize(true)
         binding.rvPostDetailPhotoList.setHasFixedSize(true)
 
-        binding.rvPostDetailPlaceList.adapter = PostDetailPlaceAdapter(placeList)
-        binding.rvPostDetailPhotoList.adapter = PostDetailPhotoAdapter//PostDetailPhotoAdapter(photoList, this)
+//        binding.rvPostDetailPlaceList.adapter = PostDetailPlaceAdapter(placeList)
 
         binding.ivPostDetailProfileImg.background = ShapeDrawable(OvalShape())
         binding.ivPostDetailProfileImg.clipToOutline = true //안드로이드 버전 5 롤리팝 이상에서만 적용
@@ -240,11 +197,7 @@ class PostDetailActivity : AppCompatActivity() {
     }
 
     // 사진 정보 가져오기 : 사진 이름 반환
-    private fun getPlacePhoto(postId : String) : ArrayList<String> {
-        val postPhotoList = ArrayList<String>()
-
-        Log.d(TAG, postId)
-
+    private fun getPlacePhoto(postId : String) {
         // 사진 이름 가져오기
         val db = Firebase.firestore
         db.collection("postPhotoT")
@@ -254,25 +207,44 @@ class PostDetailActivity : AppCompatActivity() {
                     for (document in result) {
                         val map = document.data as HashMap<String, Any>
                         val postPhoto : String = map["postPhoto"] as String
-                        Log.d(TAG + "하아", postPhoto)
-                        postPhotoList.add(postPhoto)
+                        getPhotoUri(postPhoto)
                     }
                 }
                 .addOnFailureListener { exception ->
                     Log.w(TAG, "getPlacePhoto Error getting documents: ", exception)
-                }.apply { return postPhotoList }
+                }
     }
 
     // 사진 uri 가져오기
-    private fun getPhotoUri(postPhotoList : ArrayList<String>) : ArrayList<String> {
-        val postPhotoUri = ArrayList<String>()
+    private fun getPhotoUri(postPhoto : String) {
+        storageRef.child("post/$postPhoto").downloadUrl
+                .addOnSuccessListener { uri ->
+                    postDetailPhotoAdapter.photoListUri.add(uri)
+                    postDetailPhotoAdapter.notifyDataSetChanged()
+                }
+    }
 
-        for (postPhoto in postPhotoList) {
-            storageRef.child("post/$postPhoto").downloadUrl
-                    .addOnSuccessListener { uri ->
-                        postPhotoUri.add(uri.toString())
+    // 장소 정보 가져오기 + 설정
+    private fun getPlace(postId : String) {
+        // 사진 이름 가져오기
+        val db = Firebase.firestore
+        db.collection("postPlaceT")
+                .whereEqualTo("postId", postId)
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+//                        val map = document.data as HashMap<String, Any>
+//                        val icon : Long = map["placeCategory"] as Long
+//                        val name: String = map["placeName"] as String
+//                        val location: String = map["placeLoc"] as String
+//                        if (icon == 1L) postDetailPlaceAdapter.placeList.add(ModelPostDetailPlace(R.drawable.ic_location_yellow, name, location))
+//                        else if (icon == 2L) postDetailPlaceAdapter.placeList.add(ModelPostDetailPlace(R.drawable.ic_food, name, location))
+//                        else continue
+//                        postDetailPlaceAdapter.notifyDataSetChanged()
                     }
-        }
-        return postPhotoUri
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "getPlacePhoto Error getting documents: ", exception)
+                }
     }
 }
